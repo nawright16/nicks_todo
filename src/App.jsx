561701323a499fa499179c6511e5484e92960c96
nawright16 <nawright16@gmail.com>
2024-04-21 11:02:19 -0400

    adding 'item' and setItem for state

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
+import { useState } from "react";
+import "./App.css";
 
+function App() {
+  const [item, setItem] = useState([]);
 
-import './App.css'
+  return (
+    <>
+      <div className="form">
+        <h1>Nick's Todo</h1>
+        <form>
+          <input type="text" />
+          <button className="add-btn"></button>
+        </form>
+      </div>
+      <div className="list">
+        <h1>Todos</h1>
+        <ul>
+          <li>
+            <input type="checkbox" />
+            Item 1
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
 
-
-
-  function App() {
-    return (
-      <>
-        <div className="form">
-          <h1>Nick's Todo</h1>
-          <form>
-            <input type="text" />
-            <button className="add-btn"></button>
-          </form>
-        </div>
-        <div className="list">
-          <h1>Todos</h1>
-          <ul>
-            <li>
-              <input type="checkbox" />
-              Item 1
-            </li>
-          </ul>
-        </div>
-      </>
-    );
-  }
-  
-export default App
+export default App;
