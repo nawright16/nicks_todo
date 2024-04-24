ea61305956c813fc1ca88ae0e85924ec832abd25
nawright16 <nawright16@gmail.com>
2024-04-24 15:36:14 -0400

    added todos and setTodos for state. Added handleSubmit function and addTodo function. Sending items to todos list section with todos.map method

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,42 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState('');
+  const [todos, setTodos] = useState([])
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    addTodo(item);
+    setItem("");
+    
+  }
+
+  function addTodo(item) {
+    const currentTodos = [...todos, item ];
+    setTodos(currentTodos)
+    
+  }
   return (
     <>
       <div className="form">
         <h1>Nick's Todo</h1>
-        <form>
-          <input type="text" />
-          <button className="add-btn"></button>
+        <form id="form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+          <button className="add-btn">add item</button>
         </form>
       </div>
       <div className="list">
         <h1>Todos</h1>
         <ul>
-          <li>
+          {todos.map((item, index)=> ( <li key={index}>
             <input type="checkbox" />
-            Item 1
-          </li>
+            {item}
+          </li>))}
+         
         </ul>
       </div>
     </>
